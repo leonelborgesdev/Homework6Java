bee74d095a47b3260c01a15e848f8a67151ceb1c
leonelborgesdev <leonel03t@gmail.com>
2024-01-14 19:14:19 -0400

    main listas

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,34 @@
+import jdk.jshell.execution.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        Utilidades.imprimirElementos(numeros);
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        List<String> palabras= new ArrayList<>();
+        palabras.add("Holas");
+        palabras.add("Mundo");
+        Utilidades.imprimirElementos(palabras);
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        List<Integer> numerosOrigen=new ArrayList<>();
+        numerosOrigen.add(10);
+        numerosOrigen.add(20);
+        numerosOrigen.add(30);
+
+        List<Number> numeroDestino= new ArrayList<>();
+        Utilidades.copiarElementos(numerosOrigen, numeroDestino);
+        Utilidades.imprimirElementos(numeroDestino);
+
+        List<Number> numerosFloatDestino = new ArrayList<>();
+        Utilidades.copiarElementos(numerosOrigen, numerosFloatDestino);
+        Utilidades.imprimirElementos(numerosFloatDestino);
     }
 }
